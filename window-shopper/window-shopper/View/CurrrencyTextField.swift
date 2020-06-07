9e829319edf61d823f60ac28e9c41904d793afc5
Divya Verma <diverma@adobe.com>
2020-06-08 04:46:11 +0530

    Final code

--- window-shopper/window-shopper/View/CurrrencyTextField.swift
+++ window-shopper/window-shopper/View/CurrrencyTextField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable
 class CurrrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height/2 - size/2), width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.5551366806, green: 0.5552331805, blue: 0.5551239848, alpha: 0.7092319542)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +40,8 @@ class CurrrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2478543134)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
 
         if let p = placeholder {
             let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: UIColor.white])
