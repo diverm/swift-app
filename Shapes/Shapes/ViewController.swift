e0794bfcf84ee3f85e139e911e0039e79a4fedd1
Divya Verma <diverma@adobe.com>
2020-06-07 22:39:36 +0530

    iphone and ipad layout

---
+++ Shapes/Shapes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shapes
+//
+//  Created by Divya Verma on 07/06/20.
+//  Copyright © 2020 Divya Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
